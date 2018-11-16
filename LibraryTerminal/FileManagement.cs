249a196bf214174a8222deee14446fa6e83a577e
Kristy Currier <kristy.currier@gmail.com>
2018-11-15 19:55:56 -0500

    First commit

---
+++ LibraryTerminal/FileManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTerminal
+{
+    public class FileManagement
+    {
+        //i/o file write/read lists
+    }
+}
