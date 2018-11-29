30abb3e8d455d1fa2e1fc6b0a9853df4b8f6e53f
Sebastian Wurst <sebastianwurst@quickenloans.com>
2018-11-29 17:18:18 -0500

    Validator SW

--- LibraryTerminal/Validator.cs
+++ LibraryTerminal/Validator.cs
@@ -9,7 +9,6 @@ namespace LibraryTerminal
     class Validator
     {
         //create 2 functions that takes in the user input is a title / author, return bool if it is in the list
-        //
 
         public static bool BookTitleValidator(Dictionary<int, Book> listOfBooks, string userInput)
         {
